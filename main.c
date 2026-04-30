7ae3cca596e995ec9cf45d4bda6d4662e5ba4b40
vladislavgg <saizeniberov@gmail.com>
2026-04-30 10:09:51 +0300

    Add lastname and notes

--- main.c
+++ main.c
@@ -2,10 +2,12 @@
 #include <string.h>
 int main(int argc, char **argv)
 {
-    char name[255];
-    printf("Enter your name: ");
-    fgets(name, 255, stdin);
-    name[strlen(name)-1] = '\0';
-    printf("Hello %s!\n", name);
+    char first[255], last[255];
+    printf("Enter your first name: ");
+    fgets(first, 255, stdin);
+    first[strlen(first)-1] = '\0';
+    printf("Now enter your last name: ");
+    gets(last); 
+    printf("Hello %s %s!\n", first, last);
     return 0;
 }
